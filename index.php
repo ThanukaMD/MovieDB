a166983c25b88f75b4858d1ea546a62225a4ad4d
ThanukaMD <94976220+ThanukaMD@users.noreply.github.com>
2023-04-12 14:00:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Movies Database</title>
+    <title>Movie App</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            color: #333;
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            overflow: hidden;
+            position: relative;
+        }
+
+        #bgVideo {
+            position: fixed;
+            right: 0;
+            bottom: 0;
+            min-width: 100%;
+            min-height: 100%;
+            z-index: -1;
+        }
+
+        h1 {
+            color: white;
+            font-size: 3rem;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+        }
+
+        a {
+            display: inline-block;
+            background-color: #4a86e8;
+            color: white;
+            padding: 10px 20px;
+            text-decoration: none;
+            font-size: 1.5rem;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+
+        a:hover {
+            background-color: #3c73d1;
+        }
+
+        .content {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>Movies Database</h1>
-    <a href="movies.php">View All Movies</a>
-
-    <iframe width="1550" height="700" src="https://www.youtube.com/embed/6Y3zqIL23i0" frameborder="0" allowfullscreen></iframe>
-
-    <script src="your_script.js"></script>
+    <div class="content">
+        <h1>Welcome to the Movie App</h1>
+        <a href="movies.php">View All Movies</a>
+    </div>
+    <video autoplay muted loop id="bgVideo">
+        <source src="video.mp4" type="video/mp4">
+    </video>
 </body>
 </html>
