29b5d4b34512e366849357e4409302c770ed9529
ThanukaMD <94976220+ThanukaMD@users.noreply.github.com>
2023-04-12 13:59:47 -0300

    Update movies.php

--- movies.php
+++ movies.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>All Movies</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
